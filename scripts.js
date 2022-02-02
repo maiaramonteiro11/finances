f7cffdba84fd7e605ef54560af07c1feb2cd82c6
Maiara Monteiro <maiara.monteirosilva11@gmail.com>
2022-02-02 19:07:35 -0300

    Primeiro comite

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const Modal = {
+    open(){
+      document
+      .querySelector('.modal-overlay')
+      .classList
+      .add('active')
+    },
+    close(){
+        document
+      .querySelector('.modal-overlay')
+      .classList
+      .remove('active')
+    }
+}
+
